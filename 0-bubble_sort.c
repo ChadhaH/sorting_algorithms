7543da5617a5d3717f31256a094190353e608c6e
ChadhaH <chadha.hamed1@gmail.com>
2023-10-23 14:30:07 -0700

    first task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - a function that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array to be sorted
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int loop1, loop2;
+	int temp = 0;
+
+	if (size <= 1 || array == NULL)
+		return;
+
+	for (loop1 = 0; loop1 < size; loop1++)
+	{
+		for (loop2 = 0; loop2 < size; loop2++)
+		{
+			if (array[loop2 - 1] > array[loop2])
+			{
+				temp = array[loop2 - 1];
+				array[loop2 - 1] = array[loop2];
+				array[loop2] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+
+}
